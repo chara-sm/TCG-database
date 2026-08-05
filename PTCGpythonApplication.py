e16afa2d312c9ad4c80ce40a5c26264c68c6f00f
chara-sm <22205@aic.wa.edu.au>
2026-08-05 12:09:06 +0800

    Add files via upload

--- PTCGpythonApplication.py
+++ PTCGpythonApplication.py
@@ -1,12 +1,20 @@
-import sqlite3
-our_db = "PTCGmanager.db"
+import sqlite3, hashlib, hmac, secrets
 
+# # # Constants
+OUR_DB = "PTCGmanager.db"
+HASH_SECRET_KEY = "612ftr8#%71nvvmr1BH@51i_rq2vh0H!VNIinIUH671!*(Paf]awdn"
+SEPARATOR = "---------------------------------"
+
+# # # Global variables
+logged_in = False
+
+# # # Functions
 #one time use, useless now
 def create_tables():
     with open("PTCGmanagerTables.sql", "r") as file:
         sql_script = file.read()
 
-    conn = sqlite3.connect(our_db)
+    conn = sqlite3.connect(OUR_DB)
     cursor = conn.cursor()
 
     try:
@@ -19,24 +27,61 @@ def create_tables():
     finally:
         conn.close()
 
-def hash(text):
-    hash_value = 5381 #seed
+def hash(text:str):
+    key_bytes = HASH_SECRET_KEY.encode('utf-8')
+    text_bytes = text.encode('utf-8')
 
-    for char in text:
-        hash_value = ((hash_value << 5) + hash_value) + ord(char)
+    hashed_obj = hmac.new(
+        key_bytes,
+        text_bytes,
+        hashlib.sha256
+    )
 
-    return hash_value & 0xFFFFFFFF
+    return hashed_obj.hexdigest()
 
-def login(email, password):
-    conn = sqlite3.connect(our_db)
+def login(email:str, password:str):
+    global logged_in
+
+    conn = sqlite3.connect(OUR_DB)
     cursor = conn.cursor()
 
-    query = "SELECT EXISTS (SELECT 1 FROM )"
+    hashed_password = str(hash(password))
+    
+    query = "SELECT EXISTS (SELECT 1 FROM Staff WHERE Email = ? AND PasswordHash = ?)"
+    cursor.execute(query, (email, hashed_password))
+    exists = bool(cursor.fetchone()[0])
+
+    cursor.close()  
+    conn.close()
+
+    if exists:
+        logged_in = True
+
+    return exists
 
 def logout():
     pass
 
 def main():
-    pass
+    global logged_in
+
+    print(SEPARATOR)
+    print("Weolcome to the Pokemon TCG manager!")
+
+    while True:
+        print(SEPARATOR)
+        print("Login Page")
+        user_email = input("Email:")
+        user_password = input("Password:")
+
+        login(user_email, user_password)
+
+        print(SEPARATOR)
+        if logged_in:
+            print("Successful login")
+        else:
+            print("Your details do not match anything in our system.")
+        input("> Press enter to continue ")
+        
 
 main()
